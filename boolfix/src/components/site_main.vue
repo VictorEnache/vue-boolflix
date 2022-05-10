edb27569b1a18004a6d5fbd356303842bff56ec8
VictorEnache <victor22enache@gmail.com>
2022-05-10 11:43:16 +0200

    generare array con tutte le info

--- boolfix/src/components/site_main.vue
+++ boolfix/src/components/site_main.vue
@@ -25,6 +25,23 @@
           {{ film.vote_average }}
         </div>
       </li>
+
+      <li :key='serie.id' v-for='serie in serie_tv'>
+          <div class="titolo">
+              {{serie.name}}
+          </div>
+          <div class="titolo_originale">
+              {{serie.original_name}}
+          </div>
+          <div class="lingua">
+              <img :src="`https://countryflagsapi.com/png/${serie.original_language}`" alt="" @error="imgError">
+              <div class="text">{{serie.original_language}}</div>
+              
+          </div>
+          <div class="voto">
+              {{serie.vote_average}}
+          </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -38,35 +55,45 @@ export default {
 
   data() {
     return {
-      films: "",
-      tvSeries: "",
+      films: [],
+      serie_tv: [],
       search: "",
     };
   },
 
   methods: {
+      callId(array_originale,nuovo_array){
+          for(let i = 0; i < array_originale.length; i++){
+              axios
+              .get(`https://api.themoviedb.org/3/movie/${array_originale[i].id}?api_key=588a2090952cc5fe4696e9b998b79992`)
+              .then((oggetto) =>{
+                  nuovo_array.push(oggetto.data)
+              })
+          }
+      },
+
     callApi() {
       if (this.search !== "") {
+          /* films */
         axios
         .get(
         `https://api.themoviedb.org/3/search/movie?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`
         )
         .then((oggetto) => {
-        this.films = oggetto.data.results;
-        console.log(oggetto.data.results);
+        this.callId(oggetto.data.results,this.films)
         });
-
+    /* serie_tv */
         axios
         .get(
             `https://api.themoviedb.org/3/search/tv?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`)
         .then((oggetto) => {
-        this.tvSeries = oggetto.data.results;
-        console.log(oggetto.data.results);
+        this.callId(oggetto.data.results,this.serie_tv)
         });
 
 
       }
     },
+
 
     imgError(event) {
       event.target.src = require("../assets/img/flag_placeholder.png");
