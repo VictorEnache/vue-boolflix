3f85ef773fc8cf4fcd18eeed984859341b10d13c
VictorEnache <victor22enache@gmail.com>
2022-05-10 12:20:16 +0200

    new array empty before push

--- boolfix/src/components/site_main.vue
+++ boolfix/src/components/site_main.vue
@@ -26,21 +26,24 @@
         </div>
       </li>
 
-      <li :key='serie.id' v-for='serie in serie_tv'>
-          <div class="titolo">
-              {{serie.name}}
-          </div>
-          <div class="titolo_originale">
-              {{serie.original_name}}
-          </div>
-          <div class="lingua">
-              <img :src="`https://countryflagsapi.com/png/${serie.original_language}`" alt="" @error="imgError">
-              <div class="text">{{serie.original_language}}</div>
-              
-          </div>
-          <div class="voto">
-              {{serie.vote_average}}
-          </div>
+      <li :key="serie.id" v-for="serie in serie_tv">
+        <div class="titolo">
+          {{ serie.name }}
+        </div>
+        <div class="titolo_originale">
+          {{ serie.original_name }}
+        </div>
+        <div class="lingua">
+          <img
+            :src="`https://countryflagsapi.com/png/${serie.original_language}`"
+            alt=""
+            @error="imgError"
+          />
+          <div class="text">{{ serie.original_language }}</div>
+        </div>
+        <div class="voto">
+          {{ serie.vote_average }}
+        </div>
       </li>
     </ul>
   </div>
@@ -62,44 +65,44 @@ export default {
   },
 
   methods: {
-      callId(array_originale,nuovo_array){
-          for(let i = 0; i < array_originale.length; i++){
-              axios
-              .get(`https://api.themoviedb.org/3/movie/${array_originale[i].id}?api_key=588a2090952cc5fe4696e9b998b79992`)
-              .then((oggetto) =>{
-                  nuovo_array.push(oggetto.data)
-                  
-              })
-              .catch(error => {
-                      console.log(error)
-                      nuovo_array.push(array_originale[i])
-                    })
-          }
-      },
-
-    callApi() {
-      if (this.search !== "") {
-          /* films */
+    callId(array_originale, nuovo_array) {
+      for (let i = 0; i < array_originale.length; i++) {
         axios
-        .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`
-        )
-        .then((oggetto) => {
-        this.callId(oggetto.data.results,this.films)
-        });
-    /* serie_tv */
-        axios
-        .get(
-            `https://api.themoviedb.org/3/search/tv?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`)
-        .then((oggetto) => {
-            console.log(oggetto.data.results)
-        this.callId(oggetto.data.results,this.serie_tv)
-        });
-
-
+          .get(
+            `https://api.themoviedb.org/3/movie/${array_originale[i].id}?api_key=588a2090952cc5fe4696e9b998b79992`
+          )
+          .then((oggetto) => {
+            nuovo_array = []
+            nuovo_array.push(oggetto.data);
+          })
+          .catch((error) => {
+            console.log(error);
+            nuovo_array.push(array_originale[i]);
+          });
       }
     },
 
+    callApi() {
+      if (this.search !== "") {
+        /* films */
+        axios
+          .get(
+            `https://api.themoviedb.org/3/search/movie?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`
+          )
+          .then((oggetto) => {
+            this.callId(oggetto.data.results, this.films);
+          });
+        /* serie_tv */
+        axios
+          .get(
+            `https://api.themoviedb.org/3/search/tv?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`
+          )
+          .then((oggetto) => {
+            console.log(oggetto.data.results);
+            this.callId(oggetto.data.results, this.serie_tv);
+          });
+      }
+    },
 
     imgError(event) {
       event.target.src = require("../assets/img/flag_placeholder.png");
