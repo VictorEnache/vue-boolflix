a717d8d6f4249a2adbfcb2840462ee88c1ae52ca
VictorEnache <victor22enache@gmail.com>
2022-05-11 22:10:11 +0200

    griglia

--- boolfix/src/components/site_main.vue
+++ boolfix/src/components/site_main.vue
@@ -1,61 +1,81 @@
 <template>
   <div>
-    <header>
+    <header id="site_header">
       <input type="text" v-model="search" @keyup.enter="callApi" />
       <button @click="callApi">cerca</button>
     </header>
-        <font-awesome-icon :icon='["fas", "star"]' />
-        <font-awesome-icon :icon='["far", "star"]' />
-    <ul>
-      <li :key="film.id" v-for="film in films">
-        <div class="image">
-            <img :src="`${base_url}${img_size}${film.poster_path}`" alt="">
-        </div>
-        <div class="titolo">
-          {{ film.title }}
-        </div>
-        <div class="titolo_originale">
-          {{ film.original_title }}
-        </div>
-        <div class="lingua">
-          <img
-            :src="`https://countryflagsapi.com/png/${film.original_language}`"
-            alt=""
-            @error="imgError"
-          />
-          <div class="text">{{ film.original_language }}</div>
-        </div>
-        <div class="voto" >
-          <!-- {{ Divisione(film.vote_average) }} -->
-          <font-awesome-icon v-for='number in 5' :key='number' :icon='number <= Divisione(film.vote_average)?["fas", "star"]:["far", "star"]' />
-          
-        </div>
-      </li>
+    <!-- /#site_header -->
 
-      <li :key="serie.id" v-for="serie in serie_tv">
-          <div class="image">
-              <img :src="`${base_url}${img_size}${serie.poster_path}`" alt="">
-          </div>
-        <div class="titolo">
-          {{ serie.name }}
-        </div>
-        <div class="titolo_originale">
-          {{ serie.original_name }}
-        </div>
-        <div class="lingua">
-          <img
-            :src="`https://countryflagsapi.com/png/${serie.original_language}`"
-            alt=""
-            @error="imgError"
-          />
-          <div class="text">{{ serie.original_language }}</div>
-        </div>
-        <div class="voto">
-          {{ Divisione(serie.vote_average) }}
-          
-        </div>
-      </li>
-    </ul>
+    <main id="site_main">
+      <div class="container">
+        <ul class="row">
+          <li :key="film.id" v-for="film in films" class="col">
+            <div class="image">
+              <img :src="`${base_url}${img_size}${film.poster_path}`" alt="" />
+            </div>
+            <div class="titolo">
+              {{ film.title }}
+            </div>
+            <div class="titolo_originale">
+              {{ film.original_title }}
+            </div>
+            <div class="lingua">
+              <img
+                :src="`https://countryflagsapi.com/png/${film.original_language}`"
+                alt=""
+                @error="imgError"
+              />
+              <div class="text">{{ film.original_language }}</div>
+            </div>
+            <div class="voto">
+              <!-- {{ Divisione(film.vote_average) }} -->
+              <font-awesome-icon
+                v-for="number in 5"
+                :key="number"
+                :icon="
+                  number <= Divisione(film.vote_average)
+                    ? ['fas', 'star']
+                    : ['far', 'star']
+                "
+              />
+            </div>
+          </li>
+
+          <li :key="serie.id" v-for="serie in serie_tv" class="col">
+            <div class="image">
+              <img :src="`${base_url}${img_size}${serie.poster_path}`" alt="" />
+            </div>
+            <div class="titolo">
+              {{ serie.name }}
+            </div>
+            <div class="titolo_originale">
+              {{ serie.original_name }}
+            </div>
+            <div class="lingua">
+              <img
+                :src="`https://countryflagsapi.com/png/${serie.original_language}`"
+                alt=""
+                @error="imgError"
+              />
+              <div class="text">{{ serie.original_language }}</div>
+            </div>
+            <div class="voto">
+              <font-awesome-icon
+                v-for="number in 5"
+                :key="number"
+                :icon="
+                  number <= Divisione(serie.vote_average)
+                    ? ['fas', 'star']
+                    : ['far', 'star']
+                "
+              />
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!-- /.container -->
+    </main>
+    <!-- /#site_main -->
   </div>
 </template>
 /* /template --------------------------------------------*/
@@ -71,13 +91,13 @@ export default {
       films: [],
       serie_tv: [],
       search: "",
-      base_url: 'https://image.tmdb.org/t/p/',
-      img_size: 'w200'
+      base_url: "https://image.tmdb.org/t/p/",
+      img_size: "w200",
     };
   },
 
   methods: {
-    callId(array_originale, nuovo_array,prefisso_url) {
+    callId(array_originale, nuovo_array, prefisso_url) {
       for (let i = 0; i < array_originale.length; i++) {
         axios
           .get(
@@ -85,7 +105,7 @@ export default {
           )
           .then((oggetto) => {
             nuovo_array.push(oggetto.data);
-          })
+          });
       }
     },
 
@@ -97,8 +117,12 @@ export default {
             `https://api.themoviedb.org/3/search/movie?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`
           )
           .then((oggetto) => {
-              this.films=[]
-            this.callId(oggetto.data.results, this.films,'https://api.themoviedb.org/3/movie/');
+            this.films = [];
+            this.callId(
+              oggetto.data.results,
+              this.films,
+              "https://api.themoviedb.org/3/movie/"
+            );
           });
         /* serie_tv */
         axios
@@ -106,16 +130,20 @@ export default {
             `https://api.themoviedb.org/3/search/tv?api_key=588a2090952cc5fe4696e9b998b79992&query=${this.search}`
           )
           .then((oggetto) => {
-              this.serie_tv = []
+            this.serie_tv = [];
             console.log(oggetto.data.results);
-            this.callId(oggetto.data.results, this.serie_tv,'https://api.themoviedb.org/3/tv/');
+            this.callId(
+              oggetto.data.results,
+              this.serie_tv,
+              "https://api.themoviedb.org/3/tv/"
+            );
           });
       }
     },
 
-    Divisione(numero_da_dividere){
-        let number = Math.ceil(numero_da_dividere / 2)
-        return number
+    Divisione(numero_da_dividere) {
+      let number = Math.ceil(numero_da_dividere / 2);
+      return number;
     },
 
     imgError(event) {
